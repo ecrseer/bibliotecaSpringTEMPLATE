493b086b32176255c918d8bfabfd91d70f1a04ea
Gabriel J <60169516+ecrseer@users.noreply.github.com>
2020-07-26 22:00:25 -0300

    implementando acceleration

---
+++ src/main/java/br/com/codenation/challenge/repository/AccelerationRepo.java
@@ -0,0 +1,10 @@
+package br.com.codenation.challenge.repository;
+
+import br.com.codenation.challenge.entity.Acceleration;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface AccelerationRepo extends CrudRepository<Acceleration,Long> {
+    List<Acceleration> findAll();
+}
